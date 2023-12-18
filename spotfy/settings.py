f6af4f5cc538fb506ed1318b7e9187a4608fca1e
Giyos12 <giyosoripov4@gmail.com>
2023-12-18 12:57:54 +0500

    add dockerfile

--- spotfy/settings.py
+++ spotfy/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-yp$cxfgiy^dzxmc-e8$wo)u7a7+90lyl#hf4(&ps0wno29a+cc
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
@@ -82,14 +82,31 @@ WSGI_APPLICATION = 'spotfy.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql",
+#         "NAME": "spotify",
+#         "USER": "postgres",
+#         "PASSWORD": "Giyos123!",
+#         "HOST": "127.0.0.1",
+#         "PORT": "5432",
+#     }
+# }
+# DATABASES = {
+#     # sqlite3
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }}
+
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": "spotify",
+        "NAME": "postgres",
         "USER": "postgres",
-        "PASSWORD": "Giyos123!",
-        "HOST": "127.0.0.1",
-        "PORT": "5432",
+        "PASSWORD": "postgres",
+        "HOST": "db",
+        "PORT": 5432,
     }
 }
 
